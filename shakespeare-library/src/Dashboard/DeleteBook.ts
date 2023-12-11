45f168e86c77359c84e06d6a7065d4b26d47a6f2
Giovannicampo <giovanni.campo144@gmail.com>
2023-12-11 10:12:19 +0100

    docs and fix

--- shakespeare-library/src/Dashboard/DeleteBook.ts
+++ shakespeare-library/src/Dashboard/DeleteBook.ts
@@ -3,20 +3,20 @@ import { deleteRow } from "./BooksData";
 import { BASE_URL } from "../App";
 
 export default function deleteBook(book: Book) {
-    // edit mock data
-    deleteRow(book.id);
+  // edit mock data
+  deleteRow(book.id);
 
-    // call DELETE, but it doesn't work
-    fetch(`${BASE_URL}/books/updateById/${book.id}`, {
+  // call DELETE, but it doesn't work
+  fetch(`${BASE_URL}/books/updateById/${book.id}`, {
     method: "DELETE",
     headers: {
       "Content-Type": "application/json",
       "X-RapidAPI-Key": "your-api-key",
       "X-RapidAPI-Host": "jokes-by-api-ninjas.p.rapidapi.com",
     },
-    body: JSON.stringify(book)
+    body: JSON.stringify(book),
   })
-  .then(response => response.json())
-  .then(result => console.log(result))
-  .catch(error => console.log(error));
+    .then((response) => response.json())
+    .then((result) => console.log(result))
+    .catch((error) => console.log(error));
 }
